82def0331a52eb72426a8178773726e7051af35c
RaresFZ <rares.dragomir@epitech.eu>
2024-03-21 13:08:00 +0100

    final push

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
 /*
-FF15
+** EPITECH PROJECT, 2024
+** commande.c
+** File description:
+** minishell
 */
 
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     printf("hello world\n");
     return 0;
 }
